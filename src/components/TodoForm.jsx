9176d7af54db7392ecfd7cb333ade65643e4ae33
Vidita Singh <vidita1812@gmail.com>
2025-05-16 14:11:52 +0530

    add form functionality

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { use } from 'react'
 
 function TodoForm() {
     
+  const [todo, setTodo] = useState("")
+  const {add} = useTodo()
+
+  const addTodo = (e) =>{
+    e.preventDefault()
+
+    if (!todo) {
+      return
+    }
+
+    add({todo, completed: false})
+    setTodo("")
+  }
 
     return (
-        <form  className="flex">
+        <form onSubmit={{addTodo}} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
+                value={todo}
+                onChange={(e)=>setTodo(e.target.value)}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
