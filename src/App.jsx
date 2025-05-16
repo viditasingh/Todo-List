0904a1da40297e10a15a62541302aae7c38cfe82
Vidita Singh <vidita1812@gmail.com>
2025-05-16 13:58:25 +0530

    add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,21 @@ function App() {
     setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id===id?{...prevTodo,completed: !prevTodo.completed}:prevTodo))
   }
 
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if(todos && todos.length>0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  
+  }, [todos])
+  
+  
+
   return (
     <TodoProvider value={{todos, add, complete, deleteTask,edit}}>
       <div className="bg-[#172842] min-h-screen py-8">
